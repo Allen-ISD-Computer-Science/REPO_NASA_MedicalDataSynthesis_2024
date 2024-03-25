6e4727824406819d392e6ff598e95aaa1c4dcf21
Zoe-yau <zoe.yau@student.allenisd.org>
2024-03-25 14:41:46 -0500

    chatbot almost working need to add in API Key

--- AIBot/README.md
+++ AIBot/README.md
@@ -1,8 +1,9 @@
 ## What is this
-The main.py file is the basics code that interacts with the OpenAI API
+The main.py file contains the main program to be run
+This was demonstrated at Irving
 
 # How to Use
-This code needs the correct OpenAI Python Libraries. Be sure to pip install 'requirements.txt' before use
+This code currently needs the correct OpenAI Python Libraries. Be sure to pip install 'requirements.txt' before use
 
 Even then this code will not work on pull, this is because it does not have the API key
 I cannot upload the API key to github because then anybody could see it
--- www/ChatBox/chatScript.js
+++ www/ChatBox/chatScript.js
@@ -37,89 +37,6 @@ const handleChat = () => {
 }
 
 sendChatBtn.addEventListener("click", handleChat);
-*/ 
-
-/*
-const chatbotToggler = document.querySelector(".chatbot-toggler");
-const closeBtn = document.querySelector(".close-btn");
-const chatbox = document.querySelector(".chatbox");
-const chatInput = document.querySelector(".chat-input textarea");
-const sendChatBtn = document.querySelector(".chat-input span");
-
-let userMessage = null;
-const API_KEY = "KEY GOES HERE";
-const inputInitHeight = chatInput.scrollHeight;
-
-const createChatLi = (message, className) => {
-    const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
-    chatLi.innerHTML = chatContent;
-    chatLi.querySelector("p").textContent = message;
-    return chatLi;
-}
-
-const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = chatElement.querySelector("p");
-
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-
-        })
-    }
-
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
-
-const handleChat = () => {
-    userMessage = chatInput.ariaValueMax.trim();
-    if(!userMessage) return;
-
-    chatInput.value = "";
-    chatInput.style.height = `${inputInitHeight}px`;
-
-    chatbox.appendChild(createChatLi(userMessage, "outgoing"));
-    chatbox.scrollTo(0, chatbox.scrollHeight);
-
-    setTimeout(() => {
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
-        chatbox.appendChild(incomingChatLi);
-        chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
-
-    }, 600);
-
-}
-
-chatInput.addEventListener("input", () => {
-    chatInput.style.height = `${inputInitHeight}px`;
-    chatInput.style.height = `${chatInput.scrollHeight}px`;
-})
-
-chatInput.addEventListener("keydown", (e) => {
-    if(e.key === "Enter" && !e.shiftkey && window.innerWidth > 800) {
-        e.preventDefault();
-        handleChat;
-    }
-});
-
-sendChatBtn.addEventListener("click", handleChat);
-closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
-chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
-
 */
 
 const chatbotToggler = document.querySelector(".chatbot-toggler");
@@ -129,7 +46,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "PASTE-YOUR-API-KEY"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
@@ -143,10 +60,9 @@ const createChatLi = (message, className) => {
 }
 
 const generateResponse = (chatElement) => {
-    
     const API_URL = "https://api.openai.com/v1/chat/completions";
     const messageElement = chatElement.querySelector("p");
-    
+
     // Define the properties and message for the API request
     const requestOptions = {
         method: "POST",
@@ -159,7 +75,7 @@ const generateResponse = (chatElement) => {
             messages: [{role: "user", content: userMessage}],
         })
     }
-    
+
     // Send POST request to API, get response and set the reponse as paragraph text
     fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
         messageElement.textContent = data.choices[0].message.content.trim();
@@ -172,11 +88,11 @@ const generateResponse = (chatElement) => {
 const handleChat = () => {
     userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
     if(!userMessage) return;
-    
+
     // Clear the input textarea and set its height to default
     chatInput.value = "";
     chatInput.style.height = `${inputInitHeight}px`;
-    
+
     // Append the user's message to the chatbox
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
